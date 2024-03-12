635d6e186c2c63ac2753545ecb0c45199263e880
juanc <juanc@Juan_Carlos>
2024-03-12 12:14:25 -0600

    Cra un programa en java que dependionde de que le escribamos en el textfiled es el mensaje que mostrara en pantalla

---
+++ src/saludador/Saludador.java
@@ -0,0 +1,14 @@
+
+package saludador;
+
+
+public class Saludador {
+
+   
+    public static void main(String[] args) {
+        Saludo salu = new  Saludo();
+        salu.setVisible(true);
+        salu.setLocationRelativeTo(null);
+    }
+    
+}
